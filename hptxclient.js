e2781c9629d40f375e76dcedef4551500f2dd0f2
londos <londos84@gmail.com>
2017-10-06 11:46:20 -0400

    Add Comments

--- hptxclient.js
+++ hptxclient.js
@@ -1,9 +1,10 @@
-var input = dni;
-var sampleInput = input.slice(0, 24);
+var input = dni; // dni from data file
+var sampleInput = input.slice(0, 24); //just pull out the first day to look at
 document.getElementById('output').innerHTML += "One Day DNI Sample <br>";
 document.getElementById('output').innerHTML += JSON.stringify(sampleInput);
 document.getElementById('output').innerHTML += "<br>---------<br>";
 
+// Generic method for looping over an array and applying a function to each element
 var applyLoss = function (inputArray, loss) {
   var output = inputArray.map( function(x) {
     return loss(x);
@@ -11,6 +12,7 @@ var applyLoss = function (inputArray, loss) {
   return output;
 }
 
+// Example loss functions
 var loss1 = function (x) {
   return x * 0.9;
 }
@@ -27,6 +29,7 @@ var round = function (x) {
   return +(x.toFixed(3));
 }
 
+// Running scenario is just applying a series of loss functions
 var runHptx = function (input) {
   var output = applyLoss(input, loss1);
   output = applyLoss(output, loss2);
@@ -35,32 +38,40 @@ var runHptx = function (input) {
   return output;
 }
 
+// Example of how to run scenario
 var output = runHptx(sampleInput);
 document.getElementById('output').innerHTML += "Sample Output <br>";
 document.getElementById('output').innerHTML += JSON.stringify(runHptx(sampleInput));
 document.getElementById('output').innerHTML += "<br>---------<br>";
 
-var ones = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+// Resulting output can be summed by different divisions, ex, weekly, monthy, etc...
+// outputArray and outputArray2 are just playing with different output formats.  Don't need both ultimately.
 var sumBy = function (inputArray, delta) {
   var outputArray = [];
   var outputArray2 = [];
   for (var i=0; i<inputArray.length; i = i + delta) {
     var cut = inputArray.slice(i, i + delta);
+
     var sum = cut.reduce((a, b) => a + b, 0);
-    var periodSum = {"barName":i, "sum":sum};
     outputArray.push(sum);
+
+    var periodSum = {"barName":i, "sum":sum};
     outputArray2.push(periodSum);
   }
   return outputArray2;
 }
 
+// Example of how to summarize input DNI monthly-ish
 var output = sumBy(input, 30*24);
 console.log('OUTPUT', output)
 document.getElementById('output').innerHTML += "Sample Output Summed by Month <br>";
 document.getElementById('output').innerHTML += JSON.stringify(output);
 document.getElementById('output').innerHTML += "<br>---------<br>";
 
-var outputByMonth = sumBy(input, 30*24);
+// Example of how to run scenario and summaraize results
+var runScenario = runHptx(input);
+var outputByMonth = sumBy(runScenario, 30*24);
+
 //D3 Magic:
 var svg = d3.select("svg"),
     margin = {top: 120, right: 120, bottom: 130, left: 140},

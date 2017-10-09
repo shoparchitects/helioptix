4c5dfa6b50e1d9ebd1f3f6bafd6eabba2dc7a927
londos <londos84@gmail.com>
2017-10-09 14:23:24 -0400

    optional Monthly format

--- hptxclient.js
+++ hptxclient.js
@@ -4,6 +4,21 @@ document.getElementById('output').innerHTML += "One Day DNI Sample <br>";
 document.getElementById('output').innerHTML += JSON.stringify(sampleInput);
 document.getElementById('output').innerHTML += "<br>---------<br>";
 
+var monthsJulian = [
+  {"name": "Jan", "length": 31},
+  {"name": "Feb", "length": 28},
+  {"name": "Mar", "length": 31},
+  {"name": "Apr", "length": 30},
+  {"name": "May", "length": 31},
+  {"name": "Jun", "length": 30},
+  {"name": "Jul", "length": 31},
+  {"name": "Aug", "length": 31},
+  {"name": "Sep", "length": 30},
+  {"name": "Oct", "length": 31},
+  {"name": "Nov", "length": 30},
+  {"name": "Dec", "length": 31}
+];
+
 // Generic method for looping over an array and applying a function to each element
 var applyLoss = function (inputArray, loss) {
   var output = inputArray.map( function(x) {
@@ -45,28 +60,37 @@ document.getElementById('output').innerHTML += JSON.stringify(runHptx(sampleInpu
 document.getElementById('output').innerHTML += "<br>---------<br>";
 
 // Resulting output can be summed by different divisions, ex, weekly, monthy, etc...
-var sumBy = function (inputArray, delta) {
+var sumBy = function (inputArray, increment) {
   var outputArray = [];
+  if (increment == "monthly") {
+    delta = 31 * 24;
+    var mJ = 0;
+  }
   for (var i=0; i<inputArray.length; i = i + delta) {
+    if (increment == "monthly") {
+      delta = monthsJulian[mJ].length * 24;
+      title = monthsJulian[mJ].name;
+    }
     var cut = inputArray.slice(i, i + delta);
     var sum = cut.reduce((a, b) => a + b, 0);
 
-    var periodSum = {"barName":i, "sum":sum};
+    var periodSum = {"barName":title, "sum":sum};
     outputArray.push(periodSum);
+    mJ += 1;
   }
   return outputArray;
 }
 
 // Example of how to summarize input DNI monthly-ish
-var output = sumBy(input, 30*24);
-console.log('OUTPUT', output)
+var output = sumBy(input, "monthly");
+//console.log('OUTPUT', output)
 document.getElementById('output').innerHTML += "Sample Output Summed by Month <br>";
 document.getElementById('output').innerHTML += JSON.stringify(output);
 document.getElementById('output').innerHTML += "<br>---------<br>";
 
 // Example of how to run scenario and summaraize results
 var runScenario = runHptx(input);
-var outputByMonth = sumBy(runScenario, 30*24);
+var outputByMonth = sumBy(runScenario, "monthly");
 
 //D3 Magic:
 var svg = d3.select("svg"),
